ce4caf7a9009b138882a5251ee80d317bce26f3a
Leandro Martins <leandro.martins.85@gmail.com>
2020-05-18 23:11:19 -0300

    Delete Transaction

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,22 @@
-// import AppError from '../errors/AppError';
+import { getRepository, DeleteResult } from 'typeorm';
+import Transaction from '../models/Transaction';
+import AppError from '../errors/AppError';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<DeleteResult> {
+    const transactionRepository = getRepository(Transaction);
+
+    const transaction = await transactionRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Trasaction not found.');
+    }
+
+    return transactionRepository.delete(id);
   }
 }
 
